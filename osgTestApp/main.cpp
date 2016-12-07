901d79fdd134692d8dd1b5747092c9c975a12aaf
antohal <antohal@gmail.com>
2016-12-07 11:54:54 +0300

    debug draw

--- osgTestApp/main.cpp
+++ osgTestApp/main.cpp
@@ -19,26 +19,19 @@
 #include <osgViewer/CompositeViewer>
 
 #include <osgGA/GUIEventHandler>
+#include <osg/PolygonMode>
+
+#include <math.h>
+
+#define D2R 0.0174532925f
 
 namespace
 {
-	const float g_worldRadius = 1000000.f;
-
-	const float g_minCellSize = 20.f;
-
 	const float g_nearPlane = 1.f;
 	const float g_farPlane = 1000.f;
 
 	const float g_horizontalFOV = 75.f;
 	const float g_verticalFOV = 75.f;
-
-	const float g_slowMoveSpeed = 100;
-	const float g_fastMoveSpeed = 1000;
-
-
-	CVisibilityManager*	g_visibilityManager = nullptr;
-	CResourceManager*	g_resourceManager = nullptr;
-	osgViewer::Viewer*	g_mainViewer = nullptr;
 };
 
 
@@ -52,32 +45,90 @@ public:
 		buildGeometry();
 	}
 
-	osg::Node*	getRefNode() 
+	osg::Node*	getRefNode()
 	{
 		return _referenceNode;
 	}
 
-	void setPosition(const osg::Vec3& pos) 
+	void setCamera(const osg::Vec3& pos, const osg::Vec3& dir, const osg::Vec3& up)
 	{
 		_cameraPos = pos;
+		_cameraDir = dir;
+		_cameraUp = up;
+
 		rebuildVertexArrays();
 		_geo->dirtyDisplayList();
 	}
 
 private:
 
-	void createReferenceNode() 
+	void createReferenceNode()
 	{
 		_referenceNode = new osg::Geode;
 	}
 
-	void rebuildVertexArrays() 
+	void rebuildVertexArrays()
 	{
-		if (!_points.get()) 
+		if (!_points.get())
 		{
 			_points = new osg::Vec3Array;
-			_points->resize(2);
+			_points->resize(18);
 		}
+
+		// @{ Frustum 
+		
+		// center line
+		(*_points)[0] = _cameraPos;
+		(*_points)[1] = _cameraPos + _cameraDir * g_farPlane;
+
+		osg::Vec3 left = _cameraUp ^ _cameraDir;
+
+		osg::Vec3 leftSide = _cameraPos + _cameraDir * g_farPlane + left * (g_farPlane * sinf(D2R * g_horizontalFOV / 2));
+		osg::Vec3 rightSide = _cameraPos + _cameraDir * g_farPlane - left * (g_farPlane * sinf(D2R * g_horizontalFOV / 2));
+
+		osg::Vec3 topSide = _cameraPos + _cameraDir * g_farPlane + _cameraUp * (g_farPlane * sinf(D2R * g_verticalFOV / 2));
+		osg::Vec3 bottomSide = _cameraPos + _cameraDir * g_farPlane - _cameraUp * (g_farPlane * sinf(D2R * g_verticalFOV / 2));
+
+		osg::Vec3 leftTop = leftSide + _cameraUp * (g_farPlane * sinf(D2R * g_verticalFOV / 2));
+		osg::Vec3 rightTop = rightSide + _cameraUp * (g_farPlane * sinf(D2R * g_verticalFOV / 2));
+		osg::Vec3 leftBottom = leftSide - _cameraUp * (g_farPlane * sinf(D2R * g_verticalFOV / 2));
+		osg::Vec3 rightBottom = rightSide - _cameraUp * (g_farPlane * sinf(D2R * g_verticalFOV / 2));
+
+		//cross
+		(*_points)[2] = leftSide;
+		(*_points)[3] = rightSide;
+
+		(*_points)[4] = topSide;
+		(*_points)[5] = bottomSide;
+
+		// side lines
+		(*_points)[6] = _cameraPos;
+		(*_points)[7] = rightSide;
+
+		(*_points)[8] = _cameraPos;
+		(*_points)[9] = bottomSide;
+
+		(*_points)[10] = _cameraPos;
+		(*_points)[11] = leftSide;
+
+		(*_points)[12] = _cameraPos;
+		(*_points)[13] = topSide;
+
+		// square
+		(*_points)[6] = _cameraPos;
+		(*_points)[7] = leftTop;
+
+		(*_points)[8] = _cameraPos;
+		(*_points)[9] = rightTop;
+
+		(*_points)[10] = _cameraPos;
+		(*_points)[11] = leftBottom;
+
+		(*_points)[12] = _cameraPos;
+		(*_points)[13] = rightBottom;
+
+		//@}
+
 	}
 
 	void buildGeometry()
@@ -87,12 +138,20 @@ private:
 		_geo = new osg::Geometry;
 
 		osg::ref_ptr<osg::Vec4Array> color = new osg::Vec4Array;
-		color->push_back(osg::Vec4(0.0, 0.0, 1.0, 1.0));
+		color->push_back(osg::Vec4(1.0, 1.0, 0.2, 1.0));
 
 		_geo->setVertexArray(_points.get());
 		_geo->setColorArray(color.get());
 		_geo->setColorBinding(osg::Geometry::BIND_PER_PRIMITIVE_SET);
-		_geo->addPrimitiveSet(new osg::DrawArrays(GL_LINES, 0, 2));
+		_geo->addPrimitiveSet(new osg::DrawArrays(GL_LINES, 0, _points->size()));
+
+		osg::PolygonMode * polygonMode = new osg::PolygonMode;
+		polygonMode->setMode(osg::PolygonMode::FRONT_AND_BACK, osg::PolygonMode::LINE);
+
+		_geo->getOrCreateStateSet()->setAttributeAndModes(polygonMode,
+			osg::StateAttribute::OVERRIDE | osg::StateAttribute::ON);
+
+		_geo->getOrCreateStateSet()->setMode(GL_LIGHTING, osg::StateAttribute::OFF | osg::StateAttribute::OVERRIDE);
 
 		_referenceNode->addDrawable(_geo);
 	}
@@ -101,8 +160,29 @@ private:
 	osg::ref_ptr<osg::Vec3Array>	_points;
 	osg::ref_ptr<osg::Geometry>		_geo;
 
-	osg::Vec3						_cameraPos;
+	osg::Vec3						_cameraPos = osg::Vec3(0, 0, 0);
+	osg::Vec3						_cameraDir = osg::Vec3(0, 0, 1);
+	osg::Vec3						_cameraUp = osg::Vec3(0, 1, 0);
 };
+
+namespace
+{
+	const float g_worldRadius = 1000000.f;
+
+	const float g_minCellSize = 20.f;
+
+	const float g_slowMoveSpeed = 100;
+	const float g_fastMoveSpeed = 1000;
+
+
+	CVisibilityManager*	g_visibilityManager = nullptr;
+	CResourceManager*	g_resourceManager = nullptr;
+	osgViewer::Viewer*	g_mainViewer = nullptr;
+
+	CameraDebugDraw*	g_DebugDraw = nullptr;
+};
+
+
 
 class MyCompositeViewer : public osgViewer::CompositeViewer
 {
@@ -137,6 +217,7 @@ public:
 
 		g_resourceManager->Update((float)_deltaTime);
 
+		g_DebugDraw->setCamera(eye, dir, up);
 
 		//@{ move camera
 		osg::Vec3 manCenter = _cameraManipulator->getCenter();
@@ -206,12 +287,15 @@ void initVisibilityAndResourceManagers()
 
 	g_resourceManager->AddVisibilityManager(g_visibilityManager);
 
+	g_DebugDraw = new CameraDebugDraw;
 }
 
 void destroyVisibilityAndResourceManagers()
 {
 	delete g_visibilityManager;
 	delete g_resourceManager;
+
+	delete g_DebugDraw;
 }
 
 void generateTestScene()
@@ -237,7 +321,7 @@ void loadTestSceneFile()
 {
 	//osg::Node* sceneRoot = osgDB::readNodeFile(std::string("../data/scenes/DamagedDowntown/Downtown_Damage_0.obj"));
 	//osg::Node* sceneRoot = osgDB::readNodeFile(std::string("../data/scenes/medieval_city/Medieval_City.osgb"));
-	//osg::Node* sceneRoot = osgDB::readNodeFile(std::string("../data/spheres.3ds "));
+	//osg::Node* sceneRoot = osgDB::readNodeFile(std::string("../data/spheres.3ds"));
 	osg::Node* sceneRoot = osgDB::readNodeFile(std::string("../data/scenes/compas.obj"));
 	ObjectManager::Instance().generateObjectsFromOsgNode(sceneRoot);
 }
@@ -279,6 +363,8 @@ bool KeyboardEventHandler::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIAc
 
 int main(int, char**)
 {
+	
+
 	MyCompositeViewer compositeViewer;
 
 	//-------------------
@@ -291,9 +377,6 @@ int main(int, char**)
 	loadTestSceneFile();
 
 	initVisibilityAndResourceManagers();
-
-	//osg::Matrix coordinateSwitch;
-	//coordinateSwitch(2, 2) = 1;
 
 	osg::ref_ptr<osg::MatrixTransform> rootTransform = new osg::MatrixTransform();
 
@@ -324,11 +407,17 @@ int main(int, char**)
 	//@} MainView
 
 
+	osg::ref_ptr<osg::MatrixTransform> debugRootTransform = new osg::MatrixTransform();
+
+	debugRootTransform->addChild(ObjectManager::Instance().getOsgRoot());
+	debugRootTransform->addChild(g_DebugDraw->getRefNode());
+
+
 	//-------------------
 	//@{ DebugView
 	osgViewer::Viewer* debugView = new osgViewer::Viewer;
 
-	debugView->setSceneData(rootTransform);
+	debugView->setSceneData(debugRootTransform);
 
 	osgGA::TrackballManipulator* debugCameraManipulator = new osgGA::TrackballManipulator;
 	debugCameraManipulator->setAllowThrow(false);
